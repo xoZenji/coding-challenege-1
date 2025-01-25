87c0cb1d3c07d915e3e1a23497c559a8afa1f149
Jordan Higgins <jordanhiggins@usf.edu>
2025-01-24 21:45:59 -0500

    Task 1 - Employee Information

---
+++ cc_1.js
@@ -0,0 +1,14 @@
+// Task 1: Employee Info
+// Declares the variable used to showcase the employees name as a string value.
+let employeeName = "John Smith";
+// Declares the variable used to showcase the ID of John Smith as a numeric value.
+const employeeID = "001";
+// Declares the variable used to showcase the employee is currently active within the company as a boolean value.
+var isActive = true;
+
+// Logs the type and name of the employee John Smith within the console.
+console.log("Employee Name:",employeeName,"| Type:",typeof employeeName);
+// Logs the ID of the employee and its type within the console.
+console.log("Employee ID:", employeeID,"| Type:",typeof employeeID);
+// Logs whether the employee is active or not and its type within the console.
+console.log("Active:", isActive,"| Type:",typeof isActive);
