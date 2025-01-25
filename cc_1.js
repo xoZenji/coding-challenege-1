4a81dfa187b2e64e1fbfa8b2f6d60ba38733e640
Jordan Higgins <jordanhiggins@usf.edu>
2025-01-25 13:04:24 -0500

    Task 2: Product Information

--- cc_1.js
+++ cc_1.js
@@ -12,3 +12,18 @@ console.log("Employee Name:",employeeName,"| Type:",typeof employeeName);
 console.log("Employee ID:", employeeID,"| Type:",typeof employeeID);
 // Logs whether the employee is active or not and its type within the console.
 console.log("Active:", isActive,"| Type:",typeof isActive);
+
+// Task 2: Product Info
+// Declares a variable for the products name while giving it a string value.
+let productName = "Beanies";
+// Declares a variable for the price of the product while giving it a numeric value.
+const productPrice = 10;
+// Declares a variable stating if the product is still available or not while giving it a boolean value.
+var isAvailable = true;
+
+//Logs the name of the product and its type within the console.
+console.log("Product Name:", productName,"| Type:",typeof productName)
+// Logs the price of the product and its type within the console.
+console.log("Price:", productPrice, "| Type:",typeof productPrice)
+// Logs the products availability and its type within the console.
+console.log("Available?", isAvailable,"| Type:",typeof isAvailable)
